4bb5513eb8e2401847fab1dfa2fdf1ee65401ab2
RGL <RafaelGarciaLima@outlook.com>
2018-04-27 17:15:50 -0300

    Update and rename functions.sql to First2Upper.sql

--- First2Upper.sql
+++ First2Upper.sql
@@ -1,1 +1,2 @@
+-- Text functions to convert a upper case string to lower, and then convert the first letter to upper case.
 SELECT CONCAT(UPPER(LEFT(a.primeiro_nome, 1)), RIGHT(LOWER(a.primeiro_nome), LENGTH(a.primeiro_nome)-1))  FROM ator a
